523f5cd06bf2fb704d805bc2b6664bb16f170ff9
hoangphung21 <phunghoang211195@gmail.com>
2021-01-15 15:32:23 -0500

    first commit

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -4,22 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="header">
+        <h2>My Todo List</h2>
+        <input type="text" className="myInput" placeholder="Title .." />
+        <span onClick={newElement} className="addBtn">Add</span>
+      </div>
     </div>
   );
+}
+
+function newElement() {
+  console.log("add click");
 }
 
 export default App;
